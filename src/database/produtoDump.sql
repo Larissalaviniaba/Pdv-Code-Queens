5fa9640b52ae763fa613737a49c528c5ed8b4dd1
Ariel Sena <arielsena27@gmail.com>
2023-10-18 18:39:25 -0300

    correção da coluna quantidade_estoque da tabela produtos

--- src/database/produtoDump.sql
+++ src/database/produtoDump.sql
@@ -1,7 +1,7 @@
 CREATE TABLE produtos(
     id SERIAL PRIMARY KEY,
     descricao TEXT NOT NULL,
-    quantidade_estoque TEXT NOT NULL,
+    quantidade_estoque INT NOT NULL,
     valor INT NOT NULL,
     categoria_id INT REFERENCES categorias(id)
 );
