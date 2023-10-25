0a7eb8ce842ab259466c394669aae5e10d481ff2
polianams <pmrtins@gmail.com>
2023-10-25 19:44:29 -0300

    ajustando conflitos de merge

--- src/schemas/pedidoSchema.js
+++ src/schemas/pedidoSchema.js
@@ -2,7 +2,25 @@ const joi = require("joi");
 const { errosGerais, errosProduto } = require("../constants/erroMensagens");
 
 const pedidoSchema = joi.object({
-  
+  cliente_id: joi.required().messages({
+    "any.required": errosGerais.camposObrigatorios,
+    "string.trim": errosGerais.stringComEspaço,
+    "string.empty": errosGerais.camposObrigatorios,
+  }),
+  pedido_produtos: joi
+    .array()
+    .min(1)
+    .items(
+      joi.object({
+        produto_id: joi.number().required(),
+        quantidade_produto: joi.number().required(),
+      })
+    )
+    .required()
+    .messages({
+      "array.min": "O array deve ser pelo menos um pedido.",
+      "any.required": errosGerais.camposObrigatorios,
+    }),
 });
 
 module.exports = pedidoSchema;
--- src/utils/deleteImagemUtils.js
+++ src/utils/deleteImagemUtils.js
@@ -1,27 +1,25 @@
 const knex = require("../config/knexConfig");
 const s3 = require("../config/awsConfig");
 
-// const deletarImagem = async (file, categoria_id, produtoId) => {
-//   const categoriaProduto = await knex("categorias")
-//     .select("descricao")
-//     .where({ id: categoria_id })
-//     .first();
+const deletarImagem = async (produtoId) => {
+  const excluirImagem = await knex("produtos")
+    .select("produto_imagem")
+    .where({ id: produtoId })
+    .first();
 
-//   await s3
-//     .deleteObject({
-//       Bucket: process.env.BACKBLAZE_BUCKET,
-//       Key: `produtos/${categoriaProduto.descricao}/${produtoId}/${file.originalname}`,
-//     })
-//     .promise();
-// };
+  if (excluirImagem && excluirImagem.produto_imagem) {
+    const keyImagemAntiga = new URL(
+      excluirImagem.produto_imagem
+    ).pathname.substring(1);
+    await s3
+      .deleteObject({
+        Bucket: process.env.BACKBLAZE_BUCKET,
+        Key: keyImagemAntiga,
+      })
+      .promise();
 
-const atualizarImagem = async (req, id) => {
-  await s3
-    .putObject({
-      Bucket: process.env.BACKBLAZE_BUCKET,
-      Key: `produtos/${categoriaProduto.descricao}/${produtoId}/${file.originalname}`,
-      ContentType: file.mimetype,
-      Body: file.buffer,
-    })
-    .promise();
+    console.log("Imagem antiga excluída do S3:", excluirImagem.produto_imagem);
+  }
 };
+
+module.exports = deletarImagem;
