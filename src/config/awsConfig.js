67bb464fc129ab03df77f080b8d5a48f5b186b62
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2023-10-26 19:11:04 -0300

    Corrigindo erro o deploy

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -1,13 +1,13 @@
-const aws = require("aws-sdk");
+// const aws = require("aws-sdk");
 
-const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
+// const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
 
-const s3 = new aws.S3({
-  endpoint,
-  credentials: {
-    accessKeyId: process.env.KEY_ID,
-    secretAccessKey: process.env.APP_KEY,
-  },
-});
+// const s3 = new aws.S3({
+//   endpoint,
+//   credentials: {
+//     accessKeyId: process.env.KEY_ID,
+//     secretAccessKey: process.env.APP_KEY,
+//   },
+// });
 
-module.exports = s3;
+// module.exports = s3;
