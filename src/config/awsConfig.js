9222b7500194ecb25f345f2e1404571ad4a76994
Larissa Lavínia <larissa11.cedaspy@gmail.com>
2023-10-26 19:40:07 -0300

    Corrigindo erro o deploy

--- src/config/awsConfig.js
+++ src/config/awsConfig.js
@@ -1,6 +1,6 @@
 const aws = require("aws-sdk");
 
-const endpoint = new aws.Endpoint(process.env.BACKBLAZE_BUCKET);
+const endpoint = new aws.Endpoint(process.env.ENDPOINT_S3);
 
 const s3 = new aws.S3({
   endpoint,
@@ -10,13 +10,4 @@ const s3 = new aws.S3({
   },
 });
 
-const excluirImagem = async (path) => {
-  await s3
-    .deleteObject({
-      Bucket: process.env.BACKBLAZE_BUCKET_NAME,
-      Key: path,
-    })
-    .promise();
-};
-
-module.exports = { excluirImagem };
+module.exports = s3;
